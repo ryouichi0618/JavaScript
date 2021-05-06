cb2a744e332dfac7d301c9a173eaf2ac087dd410
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-05-06 16:37:25 +0900

    jQueryのイベントについて

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -35,6 +35,39 @@
 // });
 
 // 表示されている要素をjQueryで非表示にする
-$(function(){
-  $('.box1').hide(2000);
-});
+// $(function(){
+//   $('.box1').hide(2000);
+// });
+
+
+// 「マウスオーバー・マウスアウト」イベントを設定
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#FF0000'});
+//   });
+// });
+// class属性を追加する／解除
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+// 「マウスクリック」イベントを設定
+
+
+// $(function(){
+//   $('.box1').on('click', function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext')
+//   });
+// });

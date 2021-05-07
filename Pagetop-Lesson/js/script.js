88116e2e81f354e6503efb1730bd6bf3317893c0
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-05-07 23:19:44 +0900

    topページへのスクロールボタン機能の追加

---
+++ Pagetop-Lesson/js/script.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
+
+$('body, html').animate({
+  scrollTop:0
+}, 800);
+event.preventDefault();
+});
